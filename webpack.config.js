da2546b2ccac859300df35bc9fbdcf7a16b4cf90
matheusifz <matheusifz@gmail.com>
2021-12-14 17:02:58 -0300

    fix: webpack not loading .mp4

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        test: /\.(jpg|jpeg|png|gif|mp3|svg|mp4)$/,
         use: ["file-loader"],
       },
     ],
