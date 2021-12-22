c77809cdeab24faa957ece50389cf4c56a8d8c32
matheusifz <matheusifz@gmail.com>
2021-12-22 19:24:52 -0300

    feat: add navbar scroll effect

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+
 
 import {
   Nav,
@@ -14,9 +16,24 @@ import {
 } from "./NavbarElements";
 
 const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
   return (
     <>
-      <Nav>
+    <IconContext.Provider value={{color: '#fff'}}>
+      <Nav scrollNav={scrollNav}>
         <NavbarContainer>
           <NavLogo to="/">dolla</NavLogo>
           <MobileIcon onClick={toggle}>
@@ -40,7 +57,7 @@ const Navbar = ({ toggle }) => {
             <NavBtnLink to="/signin">Sign In</NavBtnLink>
           </NavBtn>
         </NavbarContainer>
-      </Nav>
+      </Nav></IconContext.Provider>
     </>
   );
 };
