978c9d96ad189f47207a6ab23db79b68427abf62
matheusifz <matheusifz@gmail.com>
2021-12-22 18:20:39 -0300

    feat: create footer component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Footer from "../components/Footer";
 
 import HeroSection from "../components/HeroSection";
 import InfoSection from "../components/InfoSection";
@@ -27,6 +28,7 @@ function Home() {
       <InfoSection {...HomeObjTwo} />
       <Services/>
       <InfoSection {...HomeObjThree} />
+      <Footer/>
     </>
   );
 }
